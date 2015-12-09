668d8bdfb1d3202b326aa5a8b13db590490410a9
KitsuneSolar <kitsune.solar@gmail.com>
2015-12-09 18:51:35 +0300

    Disable show PHP errors.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 ---------------------------------------------------------
 */
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 
 header('Content-Type: text/html; charset=utf-8');
 header('X-Powered-By: LiveStreet CMS');
